c34b5b1f30b46a6b70e5b6ee5796d4f1998dd91d
Brenden Matthews <brenden@diddyinc.com>
2013-08-15 10:53:43 -0700

    Fix StrToBool.

--- src/main/java/com/nexr/platform/hive/udf/UDFStrToBool.java
+++ src/main/java/com/nexr/platform/hive/udf/UDFStrToBool.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -44,12 +44,15 @@ public class UDFStrToBool extends UDF {
 				return null;
 			}
 			String s = text.toString().trim().toLowerCase();
-			if (s.equals("null")) {
-				return null;
+			if (s.equals("true") || s.equals("yes")) {
+				return Boolean.valueOf(true);
+			} else if (s.equals("false") || s.equals("no")) {
+				return Boolean.valueOf(false);
 			}
-			return Boolean.valueOf(s);
+			return null;
 		} catch (Exception e) {
 			return null;
 		}
 	}
 }
+
